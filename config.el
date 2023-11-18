04c2d7a9039aa75ab4fe62bf3666dec6fda01c80
Alen Kis <16632804+alenkis@users.noreply.github.com>
2023-11-17 17:09:06 -0700

    setup purescript

--- config.el
+++ config.el
@@ -41,6 +41,7 @@
 (load! "settings/setup-elm")
 (load! "settings/setup-clojure")
 (load! "settings/setup-haskell")
+(load! "settings/setup-purescript")
 
 ;;; Dired
 (map! :after dired
@@ -152,46 +153,6 @@
          '((company-capf company-files :with company-yasnippet)
            (company-dabbrev-code company-dabbrev))))
   (add-hook 'rustic-mode-hook #'my-rustic-mode-hook))
-
-;;; Purescript
-(add-hook 'purescript-mode-hook 'inferior-psci-mode)
-(require 'repl-toggle)
-(require 'psci)
-(add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
-
-(defun purescript-format-on-save ()
-  (interactive)
-  (let ((file (buffer-file-name)))
-    (shell-command-to-string (concat "purty --write " file))
-    (revert-buffer :ignore-auto :noconfirm)))
-
-
-(defun setup-purs-format ()
-  (when (eq major-mode 'purescript-mode)
-    (purescript-format-on-save)))
-
-(add-hook 'after-save-hook 'setup-purs-format)
-
-(defun query-pursuit (query &optional info)
-  (interactive
-   (let ((def (purescript-ident-at-point)))
-     (if (and def (symbolp def)) (setq def (symbol-name def)))
-     (list (read-string (if def
-                            (format "Pursuit query (default %s): " def)
-                          "Pursuit query: ")
-                        nil nil def)
-           current-prefix-arg)))
-  (let ((browse-url-browser-function 'eww-browse-url))
-    (purescript-pursuit query info))
-
-  (map!
-   :map purescript-mode-map
-   :localleader
-   (:prefix ("f" . "format")
-    :desc "format buffer" :n "b" #'purescript-format-on-save)
-   (:prefix ("o" . "open")
-    :desc "pursuit" :n "p" #'query-pursuit)))
-
 
 ;;; Copilot
 ;; accept completion from copilot and fallback to company
